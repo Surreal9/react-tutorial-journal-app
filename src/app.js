fbf4b7bc5c5c64813d1cbc878c3957fb74a633d5
John Arnold <john@johnarnold.ca>
2015-11-19 17:55:01 -0600

    Put presentation online for github pages.

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import JournalContainer from './JournalContainer';
-
-ReactDOM.render(
-  <JournalContainer />,
-  document.getElementById('app')
-);
